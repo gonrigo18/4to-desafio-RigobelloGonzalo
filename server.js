183272286d6916c69e21fbf0abb010faa73acc48
Gonzalo Rigobello <103467918+gonrigo18@users.noreply.github.com>
2022-10-27 08:39:53 -0300

    Version 2.3

--- server.js
+++ server.js
@@ -1,33 +1,45 @@
-const express = require('express')
-const { Router } = express
-const app = express()
+const express = require('express');
+const { Router } = express;
+const app = express();
 
-const routerProducts = new Router()
+const routerProducts = new Router();
 
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
+routerProducts.use(express.json());
 
 let allProducts = [];
 
 routerProducts.get('/', (req, res) => {
-    res.send(allProducts)
+    res.send(allProducts);
 })
 
 routerProducts.get('/:id', (req, res) => {
     const id = req.params.id;
-    res.send(allProducts[id])
+    res.send(allProducts[id]);
 })
 
+routerProducts.post('/', (req, res) => {
+    res.send(allProducts.push({
+
+        "id": 5,
+        "title": "SmartTV",
+        "price": 89999,
+        "thumbnail": ""
+
+    }));
+});
 
 
 
 
-app.use('/api/products', routerProducts)
+
+app.use('/api/products', routerProducts);
 const port = 8080;
 
 const server = app.listen(port, () => {
-    console.log(`Server up in http://localhost:${port}`)
-})
+    console.log(`Server up in http://localhost:${port}`);
+});
 
 server.on('error', (err) => console.log(`Server error: ${err}`));
