9ce57fe5e8b08775c2697375c74684284933c608
Gonzalo Rigobello <gonzalo.rigobello18@gmail.com>
2022-10-28 23:29:47 -0300

    Version 2.0

--- server.js
+++ server.js
@@ -1,33 +1,38 @@
 const express = require('express')
-const { Router } = express
-const app = express()
+const { Router } = express;
+const app = express();
 
-const routerProducts = new Router()
-
-
-app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 
-let allProducts = [];
+const routerProducts = new Router();
+
+routerProducts.use(express.json());
+
+const allProducts = [];
 
 routerProducts.get('/', (req, res) => {
-    res.send(allProducts)
+    res.send(allProducts);
 })
 
 routerProducts.get('/:id', (req, res) => {
     const id = req.params.id;
-    res.send(allProducts[id])
+    res.send(allProducts[id]);
+})
+
+routerProducts.post ('/', (req,res)=>{
+    const {body} = req;
+    allProducts.push(body);
+    res.send(body);
 })
 
 
+app.use('/api/products', routerProducts);
 
-
-
-app.use('/api/products', routerProducts)
 const port = 8080;
-
 const server = app.listen(port, () => {
-    console.log(`Server up in http://localhost:${port}`)
-})
+    console.log(`Server up in http://localhost:${port}`);
+});
 
 server.on('error', (err) => console.log(`Server error: ${err}`));
