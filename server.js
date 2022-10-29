baf9346d150faee5444eb00581ced3befd04f83a
Gonzalo Rigobello <gonzalo.rigobello18@gmail.com>
2022-10-28 23:40:56 -0300

    Version 2.9

--- server.js
+++ server.js
@@ -19,13 +19,27 @@ routerProducts.get('/', (req, res) => {
 routerProducts.get('/:id', (req, res) => {
     const id = req.params.id;
     res.send(allProducts[id]);
-})
+});
 
 routerProducts.post ('/', (req,res)=>{
     const {body} = req;
     allProducts.push(body);
     res.send(body);
-})
+});
+
+app.put('/api/products/:id', (req, res) => {
+    const { letter } = req.body;
+    const { id } = req.params;
+    const beforeLetter = letter[parseInt(id) - 1];
+    letter[parseInt(id) - 1] = letter;
+    res.send({ before: beforeLetter, new: letter });
+  });
+  
+  app.delete('/api/products/:id', (req, res) => {
+    const { id } = req.params;
+    const letter = letter.splice(parseInt(id) - 1, 1);
+    res.send({ deleted: letter});
+  });
 
 
 app.use('/api/products', routerProducts);
